3c74393c8c3b308265d9b7ef1355907c6ef6f4e8
palaashnaik <68655536+palaashnaik@users.noreply.github.com>
2024-09-29 16:27:07 +0530

    added imag

--- src/components/ui/dashboard/sidenav.tsx
+++ src/components/ui/dashboard/sidenav.tsx
@@ -1,16 +1,18 @@
 import Link from 'next/link';
 import NavLinks from '../nav-links';
 import { PowerIcon } from '@heroicons/react/24/outline';
+import Image from 'next/image';
 
 export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
+        className="mb-2 flex h-20 items-end justify-start rounded-md  p-4 md:h-40"
         href="/"
       >
-        <div className="w-32 text-white md:w-40">
-          Goa Pols
+        <div className="w-32  font-bold md:w-40">
+          <Image src={'src\assets\Emblem_of_Goa_Police.png'} alt="Goa Police" />  
+          Goa Police
         </div>
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
