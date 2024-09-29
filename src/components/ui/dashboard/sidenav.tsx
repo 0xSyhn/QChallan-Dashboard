1681d98391d0cb58712e1d76b0332d2c2769ce6b
travis fernandes <travisfernandes2327@gmail.com>
2024-09-29 17:03:11 +0530

    logo-added

--- src/components/ui/dashboard/sidenav.tsx
+++ src/components/ui/dashboard/sidenav.tsx
@@ -1,19 +1,23 @@
 import Link from 'next/link';
+import logo from "./../../../assets/logo.png"
 import NavLinks from '../nav-links';
 import { PowerIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
 
 export default function SideNav() {
   return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2">
+    <div className="flex h-full flex-col px-3 py-4 md:px-2 ">
       <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md  p-4 md:h-40"
+       //className="mb-4 flex h-10 items-end justify-start rounded-md  p-4 md:h-40"
         href="/"
       >
-        <div className="w-32  font-bold md:w-40">
-          <Image src={'/src/assets/Emblem_of_Goa_Police.png'} alt="Goa Police" width={250} height={250} />  
+      <div className=' pb-4 pt-2 flex justify-center items-center'>
+
+        <div className="w-32  font-bold md:w-40 gap-4  flex justify-center items-center">
+          <Image src={logo} alt="Goa Police" width={50} height={50} />  
           Goa Police
         </div>
+      </div>
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
